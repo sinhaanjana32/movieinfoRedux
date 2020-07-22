808e603d210118fb9cca96828705ed3d92073a9e
Anjana <sinhaanjana32@gmail.com>
2020-07-23 02:10:32 +0300

    Updates

---
+++ precache-manifest.93b9a739354b2d3b717cc37e6f637984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f67e13914348c775aa001fba7c988c6",
+    "url": "/movieinfoRedux/index.html"
+  },
+  {
+    "revision": "f66c184f1a1653d17091",
+    "url": "/movieinfoRedux/static/css/main.591906e1.chunk.css"
+  },
+  {
+    "revision": "bcec6188ee8e77aa7554",
+    "url": "/movieinfoRedux/static/js/2.6331a71f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieinfoRedux/static/js/2.6331a71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66c184f1a1653d17091",
+    "url": "/movieinfoRedux/static/js/main.9f0513f9.chunk.js"
+  },
+  {
+    "revision": "0c69d5429e8b8b2a348c",
+    "url": "/movieinfoRedux/static/js/runtime-main.196f4a86.js"
+  }
+]);
